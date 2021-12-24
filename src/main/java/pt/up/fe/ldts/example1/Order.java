b7e2fdf06fa6fea76f9bbb292f6670a7b702c6d6
malu1608 <mlflsalvador@gmail.com>
2021-12-24 17:56:02 +0000

    example 1 done

--- src/main/java/pt/up/fe/ldts/example1/Order.java
+++ src/main/java/pt/up/fe/ldts/example1/Order.java
@@ -1,40 +1,33 @@
 package pt.up.fe.ldts.example1;
 
+import pt.up.fe.ldts.example1.OrderLine;
+import pt.up.fe.ldts.example1.Product;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
     private List<OrderLine> lines;
-
     public Order() {
         lines = new ArrayList<>();
     }
-
     public void add(Product product, int quantity) {
         lines.add(new OrderLine(product, quantity));
     }
-
+    public boolean isElegibleForFreeDelivery() {
+        return getTotal()>100;
+    }
     private double getTotal() {
         double total = 0;
-
         for (OrderLine line : lines)
             total += line.getTotal();
-
         return total;
     }
-
-    public boolean isElegibleForFreeDelivery() {
-        return getTotal() > 100;
-    }
-
     public String printOrder() {
-        StringBuilder printBuffer = new StringBuilder();
-
+        StringBuffer printBuffer = new StringBuffer();
         for (OrderLine line : lines)
-            printBuffer.append(line.getLineString());
-
+            printBuffer.append(line.toString() + "\n");
         printBuffer.append("Total: " + getTotal());
-
         return printBuffer.toString();
     }
 }
--- src/main/java/pt/up/fe/ldts/example1/OrderLine.java
+++ src/main/java/pt/up/fe/ldts/example1/OrderLine.java
@@ -4,25 +4,34 @@ public class OrderLine {
     private Product product;
     private int quantity;
 
-    public OrderLine(Product product, int quantity) {
-        this.product = product;
-        this.quantity = quantity;
-    }
 
+    public void setProduct(Product product) {
+        this.product = product;
+    }
     public Product getProduct() {
         return this.product;
     }
 
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
     public int getQuantity() {
         return this.quantity;
     }
 
+    public OrderLine(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
     public double getTotal() {
-        return product.getPrice() * quantity;
+        return getProduct().getPrice() * getQuantity();
     }
 
     public String getLineString() {
         return product.getName() + "(x" + quantity + "): " + getTotal() + "\n";
     }
-
+    @Override
+    public String toString() {
+        return getProduct().getName() + "(x" + getQuantity() + "): " + (getTotal());
+    }
 }
